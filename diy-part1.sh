598faa58b051422fb14f78d199cf3c6b4843b56a
hibobmaster <32976627+hibobmaster@users.noreply.github.com>
2021-10-17 08:50:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,9 @@
 # fw876/helloworld
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
 
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 #sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
